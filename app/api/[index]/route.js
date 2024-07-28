87dbcbf69f340d938a46af6d7c336a205d4c83df
Abhinav <abhinavsinghhg@gmail.com>
2024-07-28 17:51:12 +0530

    route updated

--- app/api/[index]/route.js
+++ app/api/[index]/route.js
@@ -1,17 +1,24 @@
 import { NextResponse } from "next/server";
+const fs = require("fs");
+const path = require("path");
 
 export async function GET(req, { params }) {
   const { index } = params;
 
-  const data = {
-    1: { message: "Index 1" },
-    2: { message: "Index 2" },
-    3: { message: "Index 3" },
-  };
+  const filePath = path.join(process.cwd(), "public", "data.json");
 
-  if (data[index]) {
-    return NextResponse.json(data[index]);
-  } else {
-    return NextResponse.json({ error: "Index not found" }, { status: 404 });
+  try {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const data = JSON.parse(fileContent);
+
+    const item = data.find((item) => item.id === parseInt(index));
+
+    if (item) {
+      return NextResponse.json(item);
+    } else {
+      return NextResponse.json({ error: "Index not found" }, { status: 404 });
+    }
+  } catch (error) {
+    return NextResponse.json({ error: "Error reading data" }, { status: 500 });
   }
 }
