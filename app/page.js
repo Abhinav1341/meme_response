7e5a220409d23756f1ee1564a3c34c63cb80f253
Abhinav <abhinavsinghhg@gmail.com>
2024-07-28 23:41:14 +0530

    Hero Created

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen bg-slate-950 font-semibold text-3xl text-gray-100 flex-col items-center justify-between p-24">
-      MEMES that show HTTP Response :)
+      <Hero />
     </main>
   );
 }
